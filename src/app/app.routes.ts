59816004e0112719257b32c62ea05230e57a813a
GuiMartos <guilhermemartos2005@gmail.com>
2023-03-30 08:50:23 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,24 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'contato1',
+    loadComponent: () => import('./contato1/contato1.page').then( m => m.Contato1Page)
+  },
+  {
+    path: 'contato2',
+    loadComponent: () => import('./contato2/contato2.page').then( m => m.Contato2Page)
+  },
+  {
+    path: 'contato3',
+    loadComponent: () => import('./contato3/contato3.page').then( m => m.Contato3Page)
+  },
+  {
+    path: 'contato4',
+    loadComponent: () => import('./contato4/contato4.page').then( m => m.Contato4Page)
+  },
+  {
+    path: 'contato5',
+    loadComponent: () => import('./contato5/contato5.page').then( m => m.Contato5Page)
+  },
 ];
